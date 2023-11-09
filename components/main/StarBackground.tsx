2406a0969453377083e89b556c9bca1ef889464e
shyamraghu <sr9993663832@gmail.com>
2023-11-10 00:56:38 +0530

    fix some error

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,36 +1,44 @@
-"use client";
-
-import React, { useState, useRef, Suspense, useContext } from "react";
+"use client"
+import React, { useRef, Suspense, useContext, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { Points, PointMaterial } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 import { ColorContext } from "@/context/colors/colorContext";
+import { Object3D } from "three";
+
 const StarBackground = (props: any) => {
   const contextValue = useContext(ColorContext);
+  const ref: React.MutableRefObject<Object3D | null> = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      // You can perform any setup based on the context value here
+    }
+  }, [contextValue]);
+
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
+
   if (!contextValue) {
     return null;
   }
-  const { color } = contextValue;
-  const ref: any = useRef();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(100000), { radius: 1.2 })
-  );
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+  const { color } = contextValue;
 
   return (
     <group rotation={[0, 1, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={random.inSphere(new Float32Array(100000), { radius: 1.2 })} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color={color && color}
           size={0.002}
           sizeAttenuation={true}
-          dethWrite={false}
+          depthWrite={false}
         />
       </Points>
     </group>
@@ -40,7 +48,7 @@ const StarBackground = (props: any) => {
 const StarsCanvas = () => (
   <div className="w-full h-auto fixed inset-0 z-[20]">
     <Canvas camera={{ position: [0, 0, 1] }}>
-      <Suspense fallback={true}>
+      <Suspense fallback={null}>
         <StarBackground />
       </Suspense>
     </Canvas>
